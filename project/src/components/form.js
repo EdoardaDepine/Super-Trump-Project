4389a8342f0edd5ccda21adbbc374ec52a05e739
EdoardaDepine <edoarda.depine@gmail.com>
2023-05-26 13:24:40 -0300

    style: ajusatando estilo

--- project/src/components/form.js
+++ project/src/components/form.js
@@ -1,5 +1,4 @@
 import React from "react";
-//import PropTypes from "prop-types";
 import Input from "./input";
 import TextArea from "./textArea";
 import Select from "./select";
@@ -100,20 +99,5 @@ class Form extends React.Component {
     );
   }
 }
-
-// Form.propTypes = {
-//   cardName: PropTypes.string.isRequired,
-//   cardDescription: PropTypes.string.isRequired,
-//   cardAttr1: PropTypes.string.isRequired,
-//   cardAttr2: PropTypes.string.isRequired,
-//   cardAttr3: PropTypes.string.isRequired,
-//   cardImage: PropTypes.string.isRequired,
-//   cardRare: PropTypes.string.isRequired,
-//   cardTrunfo: PropTypes.bool.isRequired,
-//   hasTrunfo: PropTypes.bool.isRequired,
-//   isSaveButtonDisabled: PropTypes.bool.isRequired,
-//   onInputChange: PropTypes.func.isRequired,
-//   onSaveButtonClick: PropTypes.func.isRequired,
-// };
 
 export default Form;
